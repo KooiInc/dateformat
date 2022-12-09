8e507510c3ac7f4e1ecb2bb17e8a820bb452ef47
Renzo Kooi <renzo.kooi@gmail.com>
2022-12-09 10:02:15 +0100

    Use single object

--- index.js
+++ index.js
@@ -2,38 +2,37 @@ export default DateFormatFactory();
 
 function DateFormatFactory() {
   const dtfOptions = {
-    MM:   { month: `long` },
-    M:    { month: `short` },
-    m:    { month: `numeric` },
-    mm:   { month: `2-digit` },
-    yyyy: { year: `numeric` },
-    yy:   { year: `2-digit` },
-    WD:   { weekday: `long` },
-    wd:   { weekday: `short` },
-    d:    { day: `numeric` },
-    dd:   { day: `2-digit` },
-    h:    { hour: `numeric` },
-    hh:   { hour: `2-digit` },
-    mi:   { minute: `numeric` },
-    mmi:  { minute: `2-digit` },
-    s:    { second: `numeric` },
-    ss:   { second: `2-digit` },
-    ms:   { fractionalSecondDigits: 3 },
-    tz:   { timeZoneName: `shortOffset` },
-    dl:   { locale: `default` },
+    fixed: {
+      MM:   { month: `long` },
+      M:    { month: `short` },
+      m:    { month: `numeric` },
+      mm:   { month: `2-digit` },
+      yyyy: { year: `numeric` },
+      yy:   { year: `2-digit` },
+      WD:   { weekday: `long` },
+      wd:   { weekday: `short` },
+      d:    { day: `numeric` },
+      dd:   { day: `2-digit` },
+      h:    { hour: `numeric` },
+      hh:   { hour: `2-digit` },
+      mi:   { minute: `numeric` },
+      mmi:  { minute: `2-digit` },
+      s:    { second: `numeric` },
+      ss:   { second: `2-digit` },
+      ms:   { fractionalSecondDigits: 3 },
+      tz:   { timeZoneName: `shortOffset` },
+      dl:   { locale: `default` }, },
+    dynamic:  {
+      tzn: v => ( { timeZoneName: v.slice(4) } ),
+      ds:  v => ( { dateStyle: v.slice(3) } ),
+      ts:  v => ( { timeStyle: v.slice(3) } ),
+      e:   v => ( { era: v.slice(2) } ),
+      h12: _ => ( { hour12: true } ),
+      tz:  v => ( { timeZone: v.slice(3) } ),
+      l:   v => ( { locale: v.slice(2) } ), },
     get re() {
-      const keys = Object.keys(this).filter(v => !/^re/.test(v))
-      return new RegExp(`\\b(${keys.join(`|`)})\\b`, `g`);
+      return new RegExp(`\\b(${Object.keys(this.fixed).join(`|`)})\\b`, `g`);
     },
-  };
-  const shortOpts = {
-    tzn: v => ( { timeZoneName: v.slice(4) } ),
-    ds:  v => ( { dateStyle: v.slice(3) } ),
-    ts:  v => ( { timeStyle: v.slice(3) } ),
-    e:   v => ( { era: v.slice(2) } ),
-    h12: _ => ( { hour12: true } ),
-    tz:  v => ( { timeZone: v.slice(3) } ),
-    l:   v => ( { locale: v.slice(2) } )
   };
   const extractFromTemplate = (rawTemplateString = `dtf`, plainTextIndex = 0) => ( {
     texts: rawTemplateString.match(/(?<=\{)(.+?)(?=})/g) || [],
@@ -47,16 +46,16 @@ function DateFormatFactory() {
     finalize(dtf = ``, h12 = ``, era = ``) {
       return this.formatStr
         .replace(/~(\d+?)/g, `$1`)
-        .replace(/\[(\d+?)\]/g, (_, d) => this.texts[d].trim())
+        .replace(/\[(\d+?)]/g, (_, d) => this.texts[d].trim())
         .replace(/dtf/, dtf)
         .replace(/era/, era)
         .replace(/dp\b/, h12); },
   } );
   const getOpts = (...opts) => opts?.reduce( (acc, optValue) => {
     const shortOpt = optValue.slice(0, optValue.indexOf(`:`));
-    return shortOpt in shortOpts ? {...acc, ...shortOpts[shortOpt](optValue) }
-      : optValue in dtfOptions ? { ...acc, ...dtfOptions[optValue] } : acc;
-  }, dtfOptions.dl );
+    return shortOpt in dtfOptions.dynamic ? {...acc, ...dtfOptions.dynamic[shortOpt](optValue) }
+      : optValue in dtfOptions.fixed ? { ...acc, ...dtfOptions.fixed[optValue] } : acc;
+  }, dtfOptions.fixed.dl );
 
   return (date, template, moreOptions = `l:default`) => {
     const xTemplate = extractFromTemplate(template || undefined);
@@ -71,7 +70,7 @@ function DateFormatFactory() {
     const dtf = Intl.DateTimeFormat(optsCollected.locale, optsCollected).formatToParts(date)
       .reduce( (parts, v) => ( v.type === `literal` ? parts : {...parts, [v.type]: v.value } ), {} );
     xTemplate.formatStr = xTemplate.formatStr
-      .replace(dtfOptions.re, dtUnit => dtf[Object.keys(dtfOptions[dtUnit]).shift()] || dtUnit);
+      .replace(dtfOptions.re, dtUnit => dtf[Object.keys(dtfOptions.fixed[dtUnit]).shift()] || dtUnit);
 
     return xTemplate.finalize(``, dtf.dayPeriod, dtf.era);
   };
