750d317d4303338e1cbbce354aeda021982d6acf
Renzo Kooi <renzo.kooi@gmail.com>
2022-12-11 11:18:33 +0100

    Add a wrapper for browser script

---
+++ dtFormat.js
@@ -0,0 +1,98 @@
+( function( global, factory ) {
+    if ( typeof module === "object" && typeof module.exports === "object" ) {
+      module.exports = global.document ? factory( global, true ) :
+        function( w ) {
+          if ( !w.document ) {
+            throw new Error( "Intl-DateFormatter requires a window with a document" );
+          }
+          return factory( w );
+        };
+    }
+
+    return  factory( global );
+  } ) ( typeof window !== "undefined" ? window : this, loadModule );
+
+function loadModule( window, noGlobal ) {
+  const formatter = DateFormatFactory();
+  if (window.document) {
+    window.dtFormat = formatter;
+    return;
+  }
+  return formatter;
+}
+function DateFormatFactory() {
+  const dtfOptions = {
+    fixed: {
+      MM:   { month: `long` },
+      M:    { month: `short` },
+      m:    { month: `numeric` },
+      mm:   { month: `2-digit` },
+      yyyy: { year: `numeric` },
+      yy:   { year: `2-digit` },
+      WD:   { weekday: `long` },
+      wd:   { weekday: `short` },
+      d:    { day: `numeric` },
+      dd:   { day: `2-digit` },
+      h:    { hour: `numeric` },
+      hh:   { hour: `2-digit` },
+      mi:   { minute: `numeric` },
+      mmi:  { minute: `2-digit` },
+      s:    { second: `numeric` },
+      ss:   { second: `2-digit` },
+      ms:   { fractionalSecondDigits: 3 },
+      tz:   { timeZoneName: `shortOffset` },
+      dl:   { locale: `default` },
+      h12:  { hour12: true },
+    },
+    dynamic:  {
+      tzn: v => ( { timeZoneName: v.slice(4) } ),
+      hrc: v => ( { hourCycle: `h${v.slice(4)}` } ),
+      ds:  v => ( { dateStyle: v.slice(3) } ),
+      ts:  v => ( { timeStyle: v.slice(3) } ),
+      tz:  v => ( { timeZone: v.slice(3) } ),
+      e:   v => ( { era: v.slice(2) } ),
+      l:   v => ( { locale: v.slice(2) } ),
+    },
+    get re() { return new RegExp(`\\b(${Object.keys(this.fixed).join(`|`)})\\b`, `g`); },
+  };
+  const extractFromTemplate = (rawTemplateString = `dtf`, plainTextIndex = 0) => ( {
+    texts: rawTemplateString.match(/(?<=\{)(.+?)(?=})/g) || [],
+    formatStr: ` ${ rawTemplateString
+      .replace(/(?<=\{)(.+?)(?=})/g, _ => `[${plainTextIndex++}]`)
+      .replace(/[{}]/g, ``)
+      .trim()} `,
+    get units() { return this.formatStr.match(dtfOptions.re) || []; },
+    finalize(dtf = ``, h12 = ``, era = ``) {
+      return this.formatStr
+        .replace(/~(\d+?)/g, `$1`)
+        .replace(/\[(\d+?)]/g, (_, d) => this.texts[d].trim())
+        .replace(/dtf/, dtf)
+        .replace(/era/, era)
+        .replace(/dp\b/, h12); },
+  } );
+  const getOpts = (...opts) => opts?.reduce( (acc, optValue) => {
+    const shortOpt = optValue.slice(0, optValue.indexOf(`:`));
+    return {...acc, ...(dtfOptions.dynamic[shortOpt] &&
+        dtfOptions.dynamic[shortOpt](optValue) ||
+        dtfOptions.fixed[optValue]), };
+  }, dtfOptions.fixed.dl );
+  const dtSimple = (date, xTemplate, moreOptions) => {
+    const opts = getOpts(...moreOptions.split(`,`));
+    const formatted = Intl.DateTimeFormat(opts.locale, opts).format(date);
+
+    return xTemplate.finalize(formatted);
+  };
+  const dtFormatted = (date, xTemplate, moreOptions) => {
+    const optsCollected = getOpts( ...xTemplate.units.concat(moreOptions.split(`,`)).flat() );
+    const dtf = Intl.DateTimeFormat(optsCollected.locale, optsCollected).formatToParts(date)
+      .reduce( (parts, v) => ( v.type === `literal` ? parts : {...parts, [v.type]: v.value } ), {} );
+    xTemplate.formatStr = xTemplate.formatStr
+      .replace(dtfOptions.re, dtUnit => dtf[Object.keys(dtfOptions.fixed[dtUnit]).shift()] || dtUnit);
+
+    return xTemplate.finalize(``, dtf.dayPeriod, dtf.era);
+  }
+
+  return (date, template, moreOptions = `l:default`) => (/ds:|ts:/.test(moreOptions) || !template)
+    ? dtSimple(...[date, extractFromTemplate(template || undefined), moreOptions])
+    : dtFormatted(...[date, extractFromTemplate(template || undefined), moreOptions]);
+}
