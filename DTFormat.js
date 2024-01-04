105b0bd52af936f77b521cdc3082a18f14f64d2f
Renzo Kooi <renzo.kooi@gmail.com>
2024-01-04 17:37:52 +0100

    brwsrversion

--- DTFormat.js
+++ DTFormat.js
@@ -23,7 +23,7 @@ function DateFormatFactory() {
       ms: {fractionalSecondDigits: 3},
       tz: {timeZoneName: `shortOffset`},
       dl: {locale: `default`},
-      h12: {hour12: true},
+      h12: {hour12: false},
       yn: {yearName: ``},
       ry: {relatedYear: true},
       msp: {fractionalSecond: true},
@@ -48,9 +48,9 @@ function DateFormatFactory() {
   };
   const extractFromTemplate = (rawTemplateString = `dtf`, plainTextIndex = 0) => {
     let formatStr = ` ${ rawTemplateString
-        .replace(/(?<=\{)(.+?)(?=})/g, _ => `[${plainTextIndex++}]`)
-        .replace(/[{}]/g, ``)
-        .trim() } `;
+      .replace(/(?<=\{)(.+?)(?=})/g, _ => `[${plainTextIndex++}]`)
+      .replace(/[{}]/g, ``)
+      .trim() } `;
     const texts = rawTemplateString.match(/(?<=\{)(.+?)(?=})/g) || [];
     return {
       get texts() { return texts; },
@@ -60,25 +60,27 @@ function DateFormatFactory() {
       get units() { return formatStr.match(dtfOptions.re) || []; },
       finalize(dtf = ``, h12 = ``, era = ``, yn = ``) {
         return formatStr
-          .replace(/~(\d+?)/g, `$1`)
+          .replace(/~([\d+]?)/g, `$1`)
           .replace(/dtf/, dtf)
           .replace(/era/, era)
-          .replace(/dp\b/, h12)
+          .replace(/dp\b|~dp\b/, h12)
           .replace(/yn\b/, yn)
           .replace(/\[(\d+?)]/g, (_, d) => texts[d].trim())
           .trim();
-        }
+      }
     };
   };
   const unSpacify = str => str.replace(/\s+/g, ``);
   const getOpts = (...opts) => opts?.reduce( (acc, optValue) =>
       ({...acc, ...(dtfOptions.retrieveDyn(optValue) || dtfOptions.fixed[optValue]),}),
-       dtfOptions.fixed.dl );
+    dtfOptions.fixed.dl );
   const dtNoParts = (date, xTemplate, moreOptions) => {
     const opts = getOpts(...unSpacify(moreOptions).split(`,`));
     const formatted = Intl.DateTimeFormat(opts.locale, opts).format(date);
     return xTemplate.finalize(formatted);
   };
+  const getMonthName = (forDate, locale, timeZone, shrt) =>
+    forDate.toLocaleString(locale, {timeZone: timeZone, month: shrt ? short : long});
   const dtFormatted = (date, xTemplate, moreOptions) => {
     const optsCollected = getOpts( ...xTemplate.units.concat(unSpacify(moreOptions).split(`,`)).flat() );
     const opts = {...dtfOptions.fixed};
@@ -88,15 +90,19 @@ function DateFormatFactory() {
     const dtf = Intl.DateTimeFormat(optsCollected.locale, optsCollected)
       .formatToParts(date)
       .reduce( (parts, v) =>
-          (v.type === `literal` ? parts : {...parts, [v.type]: checkNumeric(v.type, v.value) } ), {} );
+        ((v.type === `literal` && /[ ,/-]/.test(v.value)) ? parts : {...parts, [v.type]: checkNumeric(v.type, v.value) } ), {} );
     opts.ms = optsCollected.fractionalSecondDigits ? opts.msp : opts.ms;
     opts.yyyy = dtf.relatedYear ? opts.ry : opts.yyyy;
+    
     xTemplate.formatStr = xTemplate.formatStr
-      .replace(dtfOptions.re, dtUnit => dtf[Object.keys(opts[dtUnit]).shift()] || dtUnit);
-
-    return xTemplate.finalize(...[,dtf.dayPeriod, dtf.era, dtf.yearName]);
+      .replace(dtfOptions.re, dtUnit =>
+        /^(M|MM)$/.test(dtUnit)
+          ? getMonthName(date, optsCollected.locale, optsCollected.timeZone, /^M$/.test(dtUnit))
+          : dtf[Object.keys(opts[dtUnit]).shift()] || dtUnit);
+    
+    return xTemplate.finalize(...[undefined, dtf.dayPeriod, dtf.era, dtf.yearName]);
   }
-
+  
   return (date, template, moreOptions = `l:default`) => (/ds:|ts:/.test(moreOptions))
     ? dtNoParts(...[date, extractFromTemplate(undefined), moreOptions])
     : dtFormatted(...[date, extractFromTemplate(template || undefined), moreOptions]);
